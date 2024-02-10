611d98500bb82a68624350e10f20453cb37ff2e4
Billy Lin <74428719+lipeilin375@users.noreply.github.com>
2024-02-10 11:41:30 +0800

    Initial commit

--- service/config.py
+++
@@ -1,2 +0,0 @@
-# set the default running port for the service (recommend 5000)
-service_port = 5000
--- web/config.js
+++
@@ -1,5 +0,0 @@
-const host = location.hostname;
-const port = 5000;
-const server = "http://" + host + ":" + port;
-
-const version = "0.1.10"
